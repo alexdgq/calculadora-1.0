ab48c05ca552b8131b01bd02a9aae19a6e534c85
alexdgq <almonteneg41@gmail.com>
2026-02-26 12:24:46 -0400

    mi primer cambio

---
+++ mi_primera_calculadora.py
@@ -0,0 +1,54 @@
+print("==================================================")
+print("                  CACULADORA                      ")
+print("==================================================")
+
+while True:
+    
+    primer_numero = int(input('ingrese el primer numero:'))
+    
+    print('''
+      operaciones.
+      para x² segundo nomero 0
+      
+      1) +
+      2) -
+      3) X
+      4) /
+      5) //
+      6) x² 
+    ''')
+
+    operacion = int(input('operacion que desea realizar:\n'))
+   
+    segundo_numero = int(input('ingrese el segundo numero:\n'))
+    
+    if operacion ==1:
+        print(f'resultado = {primer_numero + segundo_numero}')
+        
+    elif operacion ==2:
+        print(f'resultado = {primer_numero - segundo_numero}')
+        
+    elif operacion ==3:
+        print(f'resultado = {primer_numero * segundo_numero}')
+        
+    elif operacion ==4:
+        print(f'resultado = {primer_numero / segundo_numero}')
+        
+    elif operacion ==5:
+        print(f'resultado = {primer_numero // segundo_numero}')
+       
+    elif operacion ==6:
+        print(f'resultado = { primer_numero * primer_numero}')
+        
+    salir =input('salir: y/n?\n').lower()
+    
+    if salir == 'y':
+        print('CERRANDO...')
+        break
+        
+    elif salir == 'n':
+        print('siguiente operacion.')
+    
+    
+        
+        
